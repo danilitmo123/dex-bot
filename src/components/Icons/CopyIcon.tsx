ea669ede36b9a85e3c08e0a8afd4906be9216945
danilitmo123 <danilitmo@gmail.com>
2023-05-29 13:13:38 +0300

    update packages

--- src/components/Icons/CopyIcon.tsx
+++ src/components/Icons/CopyIcon.tsx
@@ -9,19 +9,17 @@ interface CopyIconProps {
 
 const CopyIcon: React.FC<CopyIconProps> = ({text}) => {
 	return (
-		<>
-			<CopyToClipboard text={text} onCopy={() => showCopyToast(text)}>
-				<img
-					style={{
-						display: "inline",
-						width: "20px",
-						cursor: "pointer",
-					}}
-					className="me-2"
-					src={contentCopy}
-				/>
-			</CopyToClipboard>
-		</>
+		<CopyToClipboard text={text} onCopy={() => showCopyToast(text)}>
+			<img
+				style={{
+					display: "inline",
+					width: "20px",
+					cursor: "pointer",
+				}}
+				className="me-2"
+				src={contentCopy}
+			/>
+		</CopyToClipboard>
 	);
 };
 
